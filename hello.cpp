81efe7f306137dad1b73effbb48bb11161905f71
mukmonthakran <139094738+mukmonthakran@users.noreply.github.com>
2023-07-10 10:43:21 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{	cout << "student code : 66-060216-2116-0"<<endl;
+	cout << "student name : monthakran srimongkol"<<endl;
+	cout <<	"    division : information technology"<<endl;
+	cout <<	"        Year : 1"<<endl;
+	cout <<	"        Room : RB" <<endl;
+	cout <<	"         Sex : Female" <<endl;
+	cout <<	"         Age : 18" <<endl;
+	system("pause");
+	return 0;
+}
